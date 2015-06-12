1bdb2f659668fdb4e7497f05d72d2fe9869b6b48
Adam Benda <adam@adasek.cz>
2015-06-12 12:54:16 +0200

    Repaired nbproject with git, cleaned up

--- src/Edge.hpp
+++ src/Edge.hpp
@@ -8,12 +8,25 @@
 #ifndef EDGE_HPP
 #define	EDGE_HPP
 
+
 class Edge {
+
+    enum layerTypes {
+        silnice,
+        mimourovne,
+        nezpevnene_cesty,
+        pasaze,
+        chodniky,
+        vyslapane_nablizky
+    };
+    
 public:
     Edge();
     Edge(const Edge& orig);
     virtual ~Edge();
 private:
+    double distanceOrig; //Distance in minutes based on datafile
+    
 
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 
 #include <cstdlib>
 #include <iostream>
+#include "Point.hpp"
+#include "Edge.hpp"
 
 #include "boost/graph/adjacency_list.hpp"
 #include "boost/graph/topological_sort.hpp"
@@ -19,36 +21,6 @@ using namespace boost;
  * 
  */
 int main(int argc, char** argv) {
-   enum files_e { dax_h, yow_h, boz_h, zow_h, foo_cpp, 
-                 foo_o, bar_cpp, bar_o, libfoobar_a,
-                 zig_cpp, zig_o, zag_cpp, zag_o, 
-                 libzigzag_a, killerapp, N };
-
-  typedef std::pair<Point, Point> Edge;
-  Edge used_by[] = {
-    Edge(dax_h, foo_cpp), Edge(dax_h, bar_cpp), Edge(dax_h, yow_h),
-    Edge(yow_h, bar_cpp), Edge(yow_h, zag_cpp),
-    Edge(boz_h, bar_cpp), Edge(boz_h, zig_cpp), Edge(boz_h, zag_cpp),
-    Edge(zow_h, foo_cpp), 
-    Edge(foo_cpp, foo_o),
-    Edge(foo_o, libfoobar_a),
-    Edge(bar_cpp, bar_o),
-    Edge(bar_o, libfoobar_a),
-    Edge(libfoobar_a, libzigzag_a),
-    Edge(zig_cpp, zig_o),
-    Edge(zig_o, libzigzag_a),
-    Edge(zag_cpp, zag_o),
-    Edge(zag_o, libzigzag_a),
-    Edge(libzigzag_a, killerapp)
-  };
-  
-  //N=number of Vertices
-  
-  typedef adjacency_list<vecS, vecS, bidirectionalS, 
-      property<vertex_color_t, default_color_type>
-    > Graph;
-  Graph g(used_by, used_by + sizeof(used_by) / sizeof(Edge), N);
-  typedef graph_traits<Graph>::vertex_descriptor Vertex;
   
   
     return 0;
